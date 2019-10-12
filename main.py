7edec8bac5c33ed0a2152210cdf704a18c316069
David Biertimpel <david.biertimpel@protonmail.com>
2019-10-12 14:37:52 +0200

    Small fixes, experimented with mountain car

--- main.py
+++ main.py
@@ -10,28 +10,36 @@ from src import train, helpers
 
 def start_training(device):
     environments = ["CartPole-v0", "MountainCar-v0"]
+    # names for saving the models afterwards
+    model_names = [("actor_cartpole", "v_cartpole"), ("actor_mountainCar", "v_mountainCar")]
+
+    learning_rates = [(0.001, 0.001), (0.00002, 0.001)]
+
     model_types = ["TD", "Q", "Advantage"]
 
     # TODO: Some more elaborated loop where we organize our experiments
 
-    env = gym.make(environments[1])
+    # later in loop
+    no_experiment = 0
+
+    env = gym.make(environments[no_experiment])
+    model_names = model_names[no_experiment]
+    learn_rates = learning_rates[no_experiment]
+
     num_episodes = 400
     gamma = 0.99
-    learn_rate = 0.001
 
     seed = 42
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     env.seed(seed)
-    n_step = 3
+    n_step = 5
 
-    model_names = "actor_mountainCar", "v_mountainCar"
-
-
-    # Decided to model actor and critic in two seperate classes
-    models, optimizer = helpers.init_model(model_types[0], env, learn_rate, device, n_hidden=(128, 256))
-    performance, cumulative_reward = train.train_actor_critic(env, models, optimizer, num_episodes, gamma, n_step, model_names)
+    # Decided to model actorlearning_rates and critic in two seperate classes
+    models, optimizer = helpers.init_model(model_types[0], env, learn_rates, device, n_hidden=(128, 256))
+    performance, cumulative_reward = train.train_actor_critic(env, models, optimizer, num_episodes, gamma, n_step,
+                                                              model_names)
 
     helpers.plot_results(performance, cumulative_reward)
 
@@ -40,8 +48,9 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     start_training(device)
 
-    model_path = "models/actor_mountainCar.pth"
-    # environment_name = "CartPole-v0"
-    environment_name = "MountainCar-v0"
-    helpers.visualize_performance(environment_name, model_path, device)
+    # environment_name = "MountainCar-v0"
+    # model_path = "models/actor_mountainCar.pth"
 
+    model_path = "models/actor_cartpole.pth"
+    environment_name = "CartPole-v0"
+    helpers.visualize_performance(environment_name, model_path, device)
--- src/helpers.py
+++ src/helpers.py
@@ -9,6 +9,7 @@ import gym
 # from .actor_critic import ValueFunction, QValueFunction, Actor
 from .actor_critic import ValueFunction, QValueFunction, Actor
 
+
 def smooth(x, N):
     """
 
@@ -26,7 +27,8 @@ def smooth(x, N):
 def plot_results(episode_durations, reward_across_episodes):
     """
 
-    :param results:
+    :param episode_durations:
+    :param reward_across_episodes:
     :return:
     """
 
@@ -40,7 +42,7 @@ def plot_results(episode_durations, reward_across_episodes):
     plt.show()
 
 
-def init_model(model_type, env, learn_rate, device, n_hidden=(128, 256)):
+def init_model(model_type, env, learn_rates, device, n_hidden=(128, 256)):
     """
 
     :param model_type:
@@ -50,18 +52,19 @@ def init_model(model_type, env, learn_rate, device, n_hidden=(128, 256)):
     :param n_hidden:
     :return:
     """
+    lr_actor, lr_critic = learn_rates
+    print("Observation and action space: ")
+    print(env.observation_space)
+    print(env.action_space)
+    n_state_features = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
     if model_type == "TD":
-
-        n_state_features = env.observation_space.shape[0]
-        n_actions = env.action_space.n
-
-        # actor = Actor(n_state_features, n_actions, n_hidden)
         actor = Actor(n_state_features, n_actions, n_hidden, device).to(device)
-        # critic = Critic(n_state_features, n_hidden)
         critic = ValueFunction(n_state_features, n_hidden, device).to(device)
 
-        opt_actor = optim.Adam(actor.parameters(), learn_rate)
-        opt_critic = optim.Adam(critic.parameters(), learn_rate)
+        opt_actor = optim.Adam(actor.parameters(), lr_actor)
+        opt_critic = optim.Adam(critic.parameters(), lr_critic)
 
         return (actor, critic), (opt_actor, opt_critic)
 
